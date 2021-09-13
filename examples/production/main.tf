b566187bbd1107c4fa6289c5d5a83e6ea0677e70
Sion Smith <sion.smith@gmail.com>
2021-09-13 18:04:37 +0100

    fixed tf tests

--- examples/production/main.tf
+++ examples/production/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.71.0"
+      version = "~>2.55.0"
     }
   }
 
@@ -10,6 +10,7 @@ terraform {
   backend "azurerm" {
   }
 }
+
 data terraform_remote_state shared {
   backend = "azurerm"
   config = {
